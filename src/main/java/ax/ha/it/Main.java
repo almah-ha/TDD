d05a1e95b659e6d78567ddc35ab53432a8b891df
almah-ha <alma.holmstrom@ha.ax>
2025-10-22 11:02:43 +0300

    Red test

--- src/main/java/ax/ha/it/Main.java
+++ src/main/java/ax/ha/it/Main.java
@@ -1,7 +1,5 @@
 package ax.ha.it;
 
 public class Main {
-    public static void main(String[] args) {
 
-    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
+    @Test
+    void shouldAddTwoPositiveNumbers() {
+        Calculator calculator = new Calculator();
+        int result = add.calculator(1, 1);
+        assertThat(result).isEqualto(2);
+    }
 
 }
