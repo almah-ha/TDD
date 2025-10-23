4a8d578f89139538c9f05302bc49f679143b3b4d
almah-ha <alma.holmstrom@ha.ax>
2025-10-23 08:46:27 +0300

    Green test

--- src/test/java/ax/ha/it/CalculatorTest.java
+++ src/test/java/ax/ha/it/CalculatorTest.java
@@ -1,14 +1,18 @@
+package ax.ha.it;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class CalculatorTest {
 
     @Test
-    public void shouldAddTwoPositiveNumbers() {
+    void shouldAddTwoPositiveNumbers() {
         Calculator calculator = new Calculator();
         int result = calculator.add(1, 1);
         assertThat(result).isEqualTo(2);
     }
+
 
 }
