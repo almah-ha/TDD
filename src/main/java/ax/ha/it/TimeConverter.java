a35683879ffa58c48064f014594623cc8be6b7be
almah-ha <alma.holmstrom@ha.ax>
2025-10-28 10:32:40 +0200

    should convert 60 seconds to minute, green test

--- src/main/java/ax/ha/it/TimeConverter.java
+++ src/main/java/ax/ha/it/TimeConverter.java
@@ -1,4 +1,11 @@
 package ax.ha.it;
 
 public class TimeConverter {
+
+    private static final int SECONDS_PER_MINUTE = 60;
+
+    public static double secondsToMinutes(long seconds) {
+        return (double) seconds / SECONDS_PER_MINUTE;
+    }
+
 }
--- src/test/java/ax/ha/it/TimeConverterTest.java
+++ src/test/java/ax/ha/it/TimeConverterTest.java
@@ -2,11 +2,14 @@ package ax.ha.it;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TimeConverterTest {
 
     @Test
     void shouldConvert60SecondsToMinute() {
         TimeConverter converter = new TimeConverter();
         assertThat(converter.secondsToMinutes(60)).isEqualTo(1.0);
+
     }
 }
