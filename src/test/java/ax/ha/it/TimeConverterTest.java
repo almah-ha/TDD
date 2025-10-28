ebc34dd735d4cae72dcd09d6dcb5c7cbf23f6a82
almah-ha <alma.holmstrom@ha.ax>
2025-10-28 10:43:47 +0200

    should convert 60 seconds to minute, refactor

--- src/test/java/ax/ha/it/TimeConverterTest.java
+++ src/test/java/ax/ha/it/TimeConverterTest.java
@@ -1,15 +1,21 @@
 package ax.ha.it;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TimeConverterTest {
 
+    private TimeConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new TimeConverter();
+    }
+
     @Test
     void shouldConvert60SecondsToMinute() {
-        TimeConverter converter = new TimeConverter();
         assertThat(converter.secondsToMinutes(60)).isEqualTo(1.0);
-
     }
 }
