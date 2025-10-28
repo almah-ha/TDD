216bc0d7fcfe8a8e8b1de57a3623e57c86aa1544
almah-ha <alma.holmstrom@ha.ax>
2025-10-28 10:51:59 +0200

    should throw exception for negative seconds, red test

--- src/test/java/ax/ha/it/TimeConverterTest.java
+++ src/test/java/ax/ha/it/TimeConverterTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TimeConverterTest {
 
@@ -18,4 +19,13 @@ public class TimeConverterTest {
     void shouldConvert60SecondsToMinute() {
         assertThat(converter.secondsToMinutes(60)).isEqualTo(1.0);
     }
+
+    @Test
+    void shouldThrowExceptionForNegativeSeconds() {
+        assertThatThrownBy(() -> converter.secondsToMinutes(-10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Seconds must be between 0 and 59");
+    }
+
+
 }
