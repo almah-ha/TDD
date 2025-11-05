18d2c4ec2fa2d556b0fb0a74068cd3ceeb1b75e1
almah-ha <alma.holmstrom@ha.ax>
2025-11-05 08:20:38 +0200

    should convert 60 minutes to hours, green test

--- src/main/java/ax/ha/it/TimeConverter.java
+++ src/main/java/ax/ha/it/TimeConverter.java
@@ -3,6 +3,7 @@ package ax.ha.it;
 public class TimeConverter {
 
     private static final int SECONDS_PER_MINUTE = 60;
+    private static final int MINUTES_PER_HOUR = 60;
 
     public static double secondsToMinutes(long seconds) {
         validateNonNegative(seconds, "seconds");
@@ -18,5 +19,10 @@ public class TimeConverter {
     public static double minutesToSeconds(long minutes) {
         validateNonNegative(minutes, "minutes");
         return (double) minutes * SECONDS_PER_MINUTE;
+    }
+
+    public static double minutesToHours(long minutes) {
+        validateNonNegative(minutes, "minutes");
+        return (double) minutes / MINUTES_PER_HOUR;
     }
 }
