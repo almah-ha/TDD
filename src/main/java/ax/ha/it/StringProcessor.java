83d81652a05a93629f223a09863a216b2f3997eb
almah-ha <alma.holmstrom@ha.ax>
2025-11-13 14:55:40 +0200

    should compress strings, green test

--- src/main/java/ax/ha/it/StringProcessor.java
+++ src/main/java/ax/ha/it/StringProcessor.java
@@ -13,4 +13,25 @@ public class StringProcessor {
         return formattedInput.equals(new StringBuilder(formattedInput).reverse().toString());
     }
 
+    public String compress(String input) {
+        if (input == null || input.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+        char[] chars = input.toCharArray();
+        int count = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                count++;
+            } else {
+                result.append(chars[i - 1]).append(count);
+                count = 1;
+            }
+        }
+
+        result.append(chars[chars.length - 1]).append(count);
+        return result.toString();
+    }
 }
